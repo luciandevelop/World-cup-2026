c431316e5f07396d6b486ac474840b07d71c3a6f
luciandevelop <luciavram87@gmail.com>
2026-06-09 13:49:12 +0300

    Delete AdminScreen.jsx

--- AdminScreen.jsx
+++
@@ -1,3 +0,0 @@
-// Compatibility wrapper: real admin screen is in src/screens/AdminScreen.jsx
-export { default } from './src/screens/AdminScreen.jsx';
-export * from './src/screens/AdminScreen.jsx';
